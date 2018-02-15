f6ea4aa58f0283c2b9d2d0e9e0318d8884a39efa
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-02-15 03:48:17 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//first version
+
+var a = 4,      	//baseline
+	h = 3,			//height
+	triangleArea;
+	
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+//second version
+
+/*var triangleBase = prompt ( 'Enter base of triangle' );
+var triangleHeight = prompt ( 'Enter height of triangle' );
+
+var triangleArea;
+triangleArea = triangleBaseline*triangleHeight/2;
+console.log('Triangle field is equal to: ' + triangleArea);*/
